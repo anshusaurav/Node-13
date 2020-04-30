607a65598f837cd6b92eadcf1f1afd0cb6b79c35
anshusaurav <anshu.saurav@gmail.com>
2020-04-30 09:24:06 +0530

    Complete-4-4

--- day4/Question-2/app.js
+++ day4/Question-2/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const http = require('http');
 var app = express();
 // app.use(logger("short"));
-app.get((req, res)=>{
+app.get((req, res,next)=>{
     console.log("In comes a " + req.method + " to " + req.url);
-    
+    res.send('Welcome Home');
 });
 
 app.listen(3000, ()=>console.log('Server is running'));
--- day4/Question-4/app.js
+++ day4/Question-4/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const http = require('http');
 var app = express();
-// app.use(logger("short"));
-app.get((req, res,next)=>{
-    console.log(Date.now());
-    next();
+
+app.use(express.json());
+app.post('/', (req, res,next )=>{
+
+    res.json(req.body);
 });
 
 app.listen(3000, ()=>console.log('Server is running'));
